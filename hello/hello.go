5d27eb28771fbfb4f25ecd0a7b02951eeb1d07cd
Medit <medit.ding@homepartners.tech>
2021-03-07 16:57:27 +0800

    update hello

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package hello
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Say(name string) {
-	fmt.Printf("Hello, %s", name)
+	fmt.Printf("Hello, %s\n", name)
+	fmt.Printf("common-go version: v1.0.1")
 }
